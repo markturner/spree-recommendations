4ce4b766433906100f0e1fe27b0fde88ce0d1f34
Vijay Hawoldar <mailme@vijays.net>
2012-05-29 17:13:44 +0100

    hacky routes until discover whats going on with :id

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Spree::Core::Engine.routes.draw do
   namespace :admin do 
-   resources :products do 
-     resources :recommendations do
-       member do
-        post :select
-        post :remove
-       end
-       collection do
-         post :available
-         get :selected
-       end
-     end
-   end
+    resources :products do 
+      resources :recommendations do
+        collection do
+          delete :remove
+          post :select
+          post :available
+          get :selected
+        end
+      end
+    end
   end  
 end
