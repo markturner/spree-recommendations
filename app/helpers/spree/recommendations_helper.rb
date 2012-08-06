dc6c64abcd4c3fc2b73fe85c66d7870b2a440cec
Mark Turner <markjturner@gmail.com>
2012-08-06 16:36:59 +0100

    Removing requirement for count from recommendations, and ensuring cart recommendations are unique

--- app/helpers/spree/recommendations_helper.rb
+++ app/helpers/spree/recommendations_helper.rb
@@ -1,29 +1,20 @@
 module Spree::RecommendationsHelper
 
-  # grab a random list of recommendations (default = 3), augmented with (different) products from the same taxon
+  # grab a random list of recommendations, augmented with (different) products from the same taxon
   # NOTE: products doesn't return products from children; but wait now for PGs
-  def random_recommendations(product, count = 3)
-    recs = product.recommendations.map{|r| r.recommended_product }.take(count)
-    rest = []
-    if (diff = count - recs.count) <= 0
-      recs
+  def random_recommendations(product, count = 0)
+    if count > 0
+      recs = product.recommendations.map{|r| r.recommended_product }.take(count)
     else
-      unless product.taxons.empty?
-        rest = product.taxons.first.products.active.
-               reject {|p| p == product || recs.include?(p)}.
-               take(diff)
-      else
-        rest = []
-      end
-      recs + rest
+      recs = product.recommendations.map{|r| r.recommended_product }
     end
   end
 
   def random_cart_recommendations
     recs = @order.line_items.collect do |line_item|
-      random_recommendations(line_item.product, 3) 
+      random_recommendations(line_item.product, 3)
     end
-    recs.flatten
+    recs.flatten.uniq
   end
 
 end
